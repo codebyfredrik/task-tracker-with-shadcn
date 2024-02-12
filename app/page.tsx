5b95bb1778276e54e4db708e33f64dc7fd1f57e7
Fredrik Tibbling <50440682+codebyfredrik@users.noreply.github.com>
2024-02-12 13:51:20 +0100

    feat: reorder tasks with react stately state

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import * as React from "react";
-import { stagger, useAnimate, motion, AnimatePresence } from "framer-motion";
+import { useListData } from "react-stately";
+import {
+  useDragAndDrop,
+  ListBox,
+  ListBoxItem,
+  DropIndicator,
+  type Selection,
+} from "react-aria-components";
+import { stagger, useAnimate, motion } from "framer-motion";
 import { toast } from "sonner";
-import { Card } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
 import {
@@ -44,10 +51,14 @@ import { updateFilter } from "@/lib/filterUtils";
 
 export default function Home() {
   const [text, setText] = React.useState("");
-  const [tasks, setTasks] = React.useState<Task[]>([]);
   const [filter, setFilter] = React.useState<FilterStatus>("all");
+  const list = useListData<Task>({
+    initialItems: [],
+    getKey: (item) => item.id,
+  });
   const [deleteTaskId, setDeleteTaskId] = React.useState<string | null>(null);
   const [ref, animate] = useAnimate();
+  let [selectedKeys, setSelectedKeys] = React.useState<Selection>(new Set());
 
   const handleSubmit: React.ComponentProps<"form">["onSubmit"] = (e) => {
     e.preventDefault();
@@ -61,9 +72,8 @@ export default function Home() {
       priority: "medium",
     };
 
-    const nextTasks = [...tasks, newTask];
+    list.prepend(newTask);
 
-    setTasks(nextTasks);
     setText("");
     setFilter("all");
 
@@ -77,25 +87,27 @@ export default function Home() {
   };
 
   const handleTaskCompletedToggle = (taskId: string) => {
-    const nextTasks = tasks.map((task) =>
-      task.id === taskId ? { ...task, completed: !task.completed } : task,
+    const task = list.getItem(taskId);
+
+    // Create a new task object with the completed property toggled
+    const updatedTask = { ...task, completed: !task.completed };
+
+    // Create a new array with the updated task
+    const nextTasks = list.items.map((item) =>
+      item.id === taskId ? updatedTask : item,
     );
 
-    const task = nextTasks.find((task) => task.id === taskId);
-
-    setTasks(nextTasks);
+    // Update the task in the list
+    list.update(taskId, updatedTask);
 
     // 🟢 If every item has been checked...
     if (nextTasks.every((task) => task.completed)) {
-      const reversedTasks = [...tasks].reverse();
-      const lastCompletedTask = reversedTasks.findIndex(
-        (task) => !task.completed,
-      );
+      const lastCompletedTask = list.items.findIndex((task) => !task.completed);
       const random = Math.random();
 
       if (random < 1 / 3) {
         animate(
-          ".checkbox",
+          ".testing",
           { scale: [1, 1.25, 1] },
           {
             duration: 0.35,
@@ -104,7 +116,7 @@ export default function Home() {
         );
       } else if (random < 2 / 3) {
         animate(
-          ".checkbox",
+          ".testing",
           { x: [0, 2, -2, 0] },
           {
             duration: 0.4,
@@ -113,7 +125,7 @@ export default function Home() {
         );
       } else {
         animate(
-          ".checkbox",
+          ".testing",
           { rotate: [0, 10, -10, 0] },
           {
             duration: 0.5,
@@ -132,9 +144,12 @@ export default function Home() {
 
     if (!idToDelete) return;
 
-    const nextTasks = [...tasks].filter((task) => task.id !== idToDelete);
+    // Create a new array without the removed task
+    const nextTasks = list.items.filter((item) => item.id !== idToDelete);
 
-    setTasks(nextTasks);
+    // Remove the task from the list
+    list.remove(idToDelete);
+
     setDeleteTaskId(null);
 
     const nextFilter = updateFilter(nextTasks, filter);
@@ -147,22 +162,85 @@ export default function Home() {
     setFilter(value);
   };
 
-  const filteredAndReversedTasks = getFilteredItems(tasks, filter).reverse();
+  const filteredTasks = getFilteredItems(list.items, filter);
 
-  const completedTasks = getCompletedItems(tasks);
+  const completedTasks = getCompletedItems(list.items);
   const hasCompletedTasks = completedTasks.length > 0;
 
-  const activeTasks = getActiveItems(tasks);
+  const activeTasks = getActiveItems(list.items);
   const hasActiveTasks = activeTasks.length > 0;
 
-  const taskCount = tasks.length;
+  const taskCount = list.items.length;
   const completedTaskCountText = `${completedTasks.length} of ${taskCount} ${
     taskCount > 1 ? "tasks" : "task"
   } completed`;
 
+  let { dragAndDropHooks } = useDragAndDrop({
+    getItems: (keys) =>
+      [...keys].map((key) => ({ "text/plain": list.getItem(key)?.name })),
+    onReorder(e) {
+      if (e.target.dropPosition === "before") {
+        list.moveBefore(e.target.key, e.keys);
+      } else if (e.target.dropPosition === "after") {
+        list.moveAfter(e.target.key, e.keys);
+      }
+
+      setTimeout(() => {
+        setSelectedKeys(new Set());
+        toast.success("Tasks have been reordered");
+      }, 2000);
+    },
+    renderDragPreview(items) {
+      let displayText = items[0]["text/plain"];
+
+      if (displayText.length > 50) {
+        displayText = displayText.substring(0, 50) + "...";
+      }
+
+      return (
+        <div className="flex min-w-min justify-between gap-5 rounded-md bg-indigo-600 px-3 py-2 text-white">
+          {displayText}
+          <span className="rounded-sm bg-white px-2 text-indigo-600">
+            {items.length}
+          </span>
+        </div>
+      );
+    },
+    renderDropIndicator(target) {
+      return (
+        <motion.div layout>
+          <DropIndicator
+            target={target}
+            className={({ isDropTarget }) =>
+              `${isDropTarget ? "my-2 border border-indigo-600" : ""}`
+            }
+          />
+        </motion.div>
+      );
+    },
+  });
+
+  function renderEmptyState() {
+    if (filter === "all") {
+      return <div className="italic">{`You don't have any tasks...`}</div>;
+    }
+
+    if (filter === "completed") {
+      return (
+        <div className="italic">{`You don't have any completed tasks...`}</div>
+      );
+    }
+
+    if (filter === "active") {
+      return (
+        <div className="italic">{`You don't have any active tasks...`}</div>
+      );
+    }
+  }
+
   return (
     <div className="flex min-h-screen flex-col p-1">
-      <div className="border-3 mx-auto mt-8 w-full max-w-2xl border-green-500 px-4 md:mt-24">
+      <div className="border-3 mx-auto mt-8 w-full max-w-2xl px-4 md:mt-24">
         <h1 className="mb-4 text-3xl font-bold">Task tracker</h1>
         <div className="mb-4 flex items-center justify-between">
           <span className="text-sm text-gray-500">
@@ -180,9 +258,7 @@ export default function Home() {
               <SelectGroup>
                 <SelectLabel>Status</SelectLabel>
                 <SelectItem value="all">
-                  All{" "}
-                  {filteredAndReversedTasks.length > 0 &&
-                    `(${filteredAndReversedTasks.length})`}
+                  All {filteredTasks.length > 0 && `(${filteredTasks.length})`}
                 </SelectItem>
                 <SelectItem value="completed" disabled={!hasCompletedTasks}>
                   Completed {hasCompletedTasks && `(${completedTasks.length})`}
@@ -213,13 +289,26 @@ export default function Home() {
               </Button>
             </div>
           </form>
-          <div className="mt-6">
-            {filteredAndReversedTasks.length ? (
-              <ul ref={ref} className="space-y-4">
-                <AnimatePresence mode="popLayout" initial={false}>
-                  {filteredAndReversedTasks.map((task) => (
-                    <motion.li
-                      key={task.id}
+          <div className="mt-6" ref={ref}>
+            <ListBox
+              aria-label="Reorderable list"
+              selectionMode="multiple"
+              items={filteredTasks}
+              dragAndDropHooks={dragAndDropHooks}
+              className="space-y-3"
+              renderEmptyState={renderEmptyState}
+              selectedKeys={selectedKeys}
+              onSelectionChange={setSelectedKeys}
+            >
+              {(item) => {
+                return (
+                  <ListBoxItem
+                    key={item.id}
+                    textValue={item.name}
+                    className="selected:bg-indigo-100"
+                  >
+                    <motion.div
+                      key={item.id}
                       layout
                       initial={{ scale: 0.75, opacity: 0 }}
                       animate={{ scale: 1, opacity: 1 }}
@@ -229,70 +318,49 @@ export default function Home() {
                         stiffness: 400,
                         damping: 30,
                       }}
+                      className={`flex justify-between rounded-lg border p-5 shadow-sm dragging:z-0 dragging:bg-indigo-50 dragging:outline-none`}
                     >
-                      <Card
-                        className={`flex justify-between p-5 hover:bg-gray-50 ${
-                          task.completed ? "bg-gray-100" : ""
-                        }`}
-                      >
-                        <div className="flex items-center space-x-6">
-                          <Checkbox
-                            id={task.id}
-                            checked={task.completed}
-                            onCheckedChange={() =>
-                              handleTaskCompletedToggle(task.id)
-                            }
-                            className="checkbox"
-                          />
-                          <label
-                            htmlFor={task.id}
-                            className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${
-                              task.completed ? "line-through" : ""
-                            }`}
+                      <div className="flex items-center space-x-6">
+                        <Checkbox
+                          id={item.id}
+                          checked={item.completed}
+                          onCheckedChange={() =>
+                            handleTaskCompletedToggle(item.id)
+                          }
+                          className="testing"
+                        />
+                        <label
+                          htmlFor={item.id}
+                          className={`text-sm font-medium leading-none dragging:text-red-400 peer-disabled:cursor-not-allowed peer-disabled:opacity-70
+                          ${item.completed ? "line-through" : ""}`}
+                        >
+                          {item.name}
+                        </label>
+                      </div>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            variant="ghost"
+                            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
                           >
-                            {task.name}
-                          </label>
-                        </div>
-                        <DropdownMenu>
-                          <DropdownMenuTrigger asChild>
-                            <Button
-                              variant="ghost"
-                              className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-                            >
-                              <DotsHorizontalIcon className="h-4 w-4" />
-                              <span className="sr-only">Open menu</span>
-                            </Button>
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent
-                            align="end"
-                            className="w-[160px]"
+                            <DotsHorizontalIcon className="h-4 w-4" />
+                            <span className="sr-only">Open menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" className="w-[160px]">
+                          <DropdownMenuItem>Edit</DropdownMenuItem>
+                          <DropdownMenuItem
+                            onClick={() => setDeleteTaskId(item.id)}
                           >
-                            <DropdownMenuItem>Edit</DropdownMenuItem>
-                            <DropdownMenuItem
-                              onClick={() => setDeleteTaskId(task.id)}
-                            >
-                              Delete
-                            </DropdownMenuItem>
-                          </DropdownMenuContent>
-                        </DropdownMenu>
-                      </Card>
-                    </motion.li>
-                  ))}
-                </AnimatePresence>
-              </ul>
-            ) : (
-              <>
-                {filter === "all" && (
-                  <div className="italic">{`You don't have any tasks...`}</div>
-                )}
-                {filter === "completed" && (
-                  <div className="italic">{`You don't have any completed tasks...`}</div>
-                )}
-                {filter === "active" && (
-                  <div className="italic">{`You don't have any active tasks...`}</div>
-                )}
-              </>
-            )}
+                            Delete
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </motion.div>
+                  </ListBoxItem>
+                );
+              }}
+            </ListBox>
           </div>
           <AlertDialog
             open={Boolean(deleteTaskId)}
